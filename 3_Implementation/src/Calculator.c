e1767f92727cff427dd147c12b966d5c3d12f23a
Tushar934 <62945123+Tushar934@users.noreply.github.com>
2021-04-15 03:12:54 +0530

    Update Calculator.c

--- 3_Implementation/src/Calculator.c
+++ 3_Implementation/src/Calculator.c
@@ -5,91 +5,159 @@ typedef struct Calculator{
     float x;
     float y;
 };
-typedef struct cal{
-	int y;
-	int x;
+typedef struct Calc{
+    int a;
+    int b;
 };
-
-float add(float a, float b) {
-    return a + b;
-}
-float sub(float a, float b) {
-    return a - b;
-}
-float mul(float a, float b) {
-    return a * b;
-}
-float divi(float a, float b) {
-	if (b==0){
-	printf("Invalid ouput\n")
-}
-else{
-    return a / b;
-}
-}
-float square_r(float a){
-    return sqrt(a);
-}
-float power(float a,float b){
- return pow(a,b);
-}
-float power_2(float a,int b){
-    return pow(a,b);
-}
-float power_3(float a,int b){
-    return pow(a,b);
-}
-float power_neg(float a,int b){
-    return pow(a,b);
-}
-float power_to_10(float a,int b){
-    return pow(a,b);
-}
-float log_10(float a){
-    return log10(a);
-}
-int modu(int a,int b){
-    return a%b;
-}
-float sin_v(float a){
-    return sin(a * 3.14159 / 180);
-}
-float cos_v(float a){
-    return cos(a * 3.14159 / 180);
-}
-float tan_v(float a){
-    return tan(a * 3.14159 / 180);;
-}
-float sin_inv(float a){
-    return 1 / (sin(a * 3.14159 / 180));
-}
-float cos_inv(float a){
-    return 1 / (cos(a * 3.14159 / 180));
-}
-float tan_inv(float a){
-    return 1 / (tan(a * 3.14159 / 180));
-}
-float percent(float a,float b)
-{
-return (a*b)/100;
-}
-float fact(float a){
-    float result = 1;
-    for(int i = 1; i <= a; i++) {
-    result = result * i;
-}
-        return result;
-}
-float power_y(float a, float b){
-    return pow(a, (1/b));
-}
-float power_3(float a, float b){
-    return pow(a, (1/b));
-}
+/**
+*  addition the a and b and returns the result
+* @param[in] a
+* @param[in] b
+* @return Result of a+b
+*/
+float add(float a, float b);
+/**
+*  substracts the a and b and returns the result
+* @param[in] a
+* @param[in] b
+* @return Result of a-b
+*/
+float sub(float a, float b);
+/**
+*  multiply the a and b and returns the result
+* @param[in] a
+* @param[in] b
+* @return Result of a*b
+*/
+float mul(float a, float b) ;
+/**
+*  divide the a and b and returns the result
+* @param[in] a
+* @param[in] b
+* @return Result of a/b
+*/
+float divi(float a, float b) ;
+/**
+*  square root the a and returns the result
+* @param[in] a
+* @return Result of square root of a
+*/
+float square_r(float a);
+/**
+*  power the a ^ b and returns the result
+* @param[in] a
+* @param[in] b
+* @return Result of a^b
+*/
+float power(float a,float b);
+/**
+*  power the a ^ b and returns the result
+* @param[in] a
+* @param[in] b
+* @return Result of a^2
+*/
+float power_2(float a,int b);
+/**
+*  power the a ^ b and returns the result
+* @param[in] a
+* @param[in] b
+* @return Result of a^3
+*/
+float power_3(float a,int b);
+/**
+*  power the a ^ b and returns the result
+* @param[in] a
+* @param[in] b
+* @return Result of a^-1
+*/
+float power_neg(float a,int b);
+/**
+*  power the a ^ b and returns the result
+* @param[in] a
+* @param[in] b
+* @return Result of 10^b
+*/
+float power_to_10(float a,int b);
+/**
+*  log10 the a  and returns the result
+* @param[in] a
+* @return Result of log10(a)
+*/
+float log_10(float a);
+/**
+*  power the a % b and returns the result
+* @param[in] a
+* @param[in] b
+* @return Result of a%b
+*/
+int modu(int a,int b);
+/**
+*  sin the a  and returns the result
+* @param[in] a
+* @return Result of Sin(a)
+*/
+float sin_v(float a);
+/**
+*  Cos the a  and returns the result
+* @param[in] a
+* @return Result of Cos(a)
+*/
+float cos_v(float a);
+/**
+*  tan the a  and returns the result
+* @param[in] a
+* @return Result of Tan(a)
+*/
+float tan_v(float a);
+/**
+*  cosec the a  and returns the result
+* @param[in] a
+* @return Result of Cosec(a)
+*/
+float sin_inv(float a);
+/**
+*  sec the a  and returns the result
+* @param[in] a
+* @return Result of Sec(a)
+*/
+float cos_inv(float a);
+/**
+*  cot the a  and returns the result
+* @param[in] a
+* @return Result of Cot(a)
+*/
+float tan_inv(float a);
+/**
+*  percent the a and b  and returns the result
+* @param[in] a
+* @param[in] b
+* @return Result of percent
+*/
+float percent(float a,float b);
+/**
+*  factorial the a  and returns the result
+* @param[in] a
+* @return Result of factorial
+*/
+float fact(float a);
+/**
+*  power the a ^ 1/b   and returns the result
+* @param[in] a
+* @param[in] b
+* @return Result of a ^ 1/b
+*/
+float power_y(float a, float b);
+/**
+*  power the a ^ 1/3   and returns the result
+* @param[in] a
+* @param[in] b
+* @return Result of a ^ 1/3
+*/
+float power_3(float a, float b);
 
 int main(void) {
 	Calculator c;
-	Cal ca;
+	Calc ca;
 int choice,result1;
 float result;
 float(*funct_ptr[])(float, float) = {add, sub, mul, divi,power,percent,power_y,power_3};
@@ -232,10 +300,10 @@ break;
 
 case 16:
 printf("Enter X: ");
-scanf("%d", &ca.x);
+scanf("%d", &ca.a);
 printf("\nEnter Y: ");
-scanf("%d", &ca.y);
-result1=funct1_ptr[0](ca.x,ca.y);
+scanf("%d", &ca.b);
+result1=funct1_ptr[0](ca.a,ca.b);
 printf("\nResult: %d\n", result1);
 break;
 
@@ -288,3 +356,81 @@ printf("\nInvalid Choice!\n");
 } while(choice);
 return 0;
 }
+float add(float a, float b) {
+    return a + b;
+}
+float sub(float a, float b) {
+    return a - b;
+}
+float mul(float a, float b) {
+    return a * b;
+}
+float divi(float a, float b) {
+    if (b==0){
+	printf("Invalid ouput\n");
+}
+else{
+    return a / b;
+}
+}
+float square_r(float a){
+    return sqrt(a);
+}
+float power(float a,float b){
+ return pow(a,b);
+}
+float power_2(float a,int b){
+    return pow(a,b);
+}
+float power_3(float a,int b){
+    return pow(a,b);
+}
+float power_neg(float a,int b){
+    return pow(a,b);
+}
+float power_to_10(float a,int b){
+    return pow(a,b);
+}
+float log_10(float a){
+    return log10(a);
+}
+int modu(int a,int b){
+    return a%b;
+}
+float sin_v(float a){
+    return sin(a * 3.14159 / 180);
+}
+float cos_v(float a){
+    return cos(a * 3.14159 / 180);
+}
+float tan_v(float a){
+    return tan(a * 3.14159 / 180);;
+}
+float sin_inv(float a){
+    return 1 / (sin(a * 3.14159 / 180));
+}
+float cos_inv(float a){
+    return 1 / (cos(a * 3.14159 / 180));
+}
+float tan_inv(float a){
+    return 1 / (tan(a * 3.14159 / 180));
+}
+float percent(float a,float b)
+{
+return (a*b)/100;
+}
+float fact(float a){
+    float result = 1;
+    for(int i = 1; i <= a; i++) {
+    result = result * i;
+}
+        return result;
+}
+float power_y(float a, float b){
+    return pow(a, (1/b));
+}
+float power_3(float a, float b){
+    return pow(a, (1/b));
+}
+
+
